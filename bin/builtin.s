c20e9cb31ec61097eafd3ad0ede68e999873b205
VargVain <118346039+VargVain@users.noreply.github.com>
2023-08-27 03:05:56 +0800

    add builtin.s

---
+++ bin/builtin.s
@@ -0,0 +1,568 @@
+	.text
+	.attribute	4, 16
+	.attribute	5, "rv32i2p0"
+	.file	"builtin.c"
+	.globl	print                           # -- Begin function print
+	.p2align	2
+	.type	print,@function
+print:                                  # @print
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	sw	s0, 8(sp)                       # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 16
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	lw	a1, -16(s0)
+	lui	a0, %hi(.L.str)
+	addi	a0, a0, %lo(.L.str)
+	call	printf
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	lw	s0, 8(sp)                       # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end0:
+	.size	print, .Lfunc_end0-print
+	.cfi_endproc
+                                        # -- End function
+	.globl	println                         # -- Begin function println
+	.p2align	2
+	.type	println,@function
+println:                                # @println
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	sw	s0, 8(sp)                       # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 16
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	lw	a1, -16(s0)
+	lui	a0, %hi(.L.str.1)
+	addi	a0, a0, %lo(.L.str.1)
+	call	printf
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	lw	s0, 8(sp)                       # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end1:
+	.size	println, .Lfunc_end1-println
+	.cfi_endproc
+                                        # -- End function
+	.globl	printInt                        # -- Begin function printInt
+	.p2align	2
+	.type	printInt,@function
+printInt:                               # @printInt
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	sw	s0, 8(sp)                       # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 16
+	.cfi_def_cfa s0, 0
+	sw	a0, -12(s0)
+	lw	a1, -12(s0)
+	lui	a0, %hi(.L.str.2)
+	addi	a0, a0, %lo(.L.str.2)
+	call	printf
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	lw	s0, 8(sp)                       # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end2:
+	.size	printInt, .Lfunc_end2-printInt
+	.cfi_endproc
+                                        # -- End function
+	.globl	printlnInt                      # -- Begin function printlnInt
+	.p2align	2
+	.type	printlnInt,@function
+printlnInt:                             # @printlnInt
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	sw	s0, 8(sp)                       # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 16
+	.cfi_def_cfa s0, 0
+	sw	a0, -12(s0)
+	lw	a1, -12(s0)
+	lui	a0, %hi(.L.str.3)
+	addi	a0, a0, %lo(.L.str.3)
+	call	printf
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	lw	s0, 8(sp)                       # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end3:
+	.size	printlnInt, .Lfunc_end3-printlnInt
+	.cfi_endproc
+                                        # -- End function
+	.globl	getString                       # -- Begin function getString
+	.p2align	2
+	.type	getString,@function
+getString:                              # @getString
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	sw	s0, 8(sp)                       # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 16
+	.cfi_def_cfa s0, 0
+	li	a0, 256
+	li	a1, 0
+	call	malloc
+	sw	a0, -16(s0)
+	lw	a1, -16(s0)
+	lui	a0, %hi(.L.str)
+	addi	a0, a0, %lo(.L.str)
+	call	scanf
+	lw	a0, -16(s0)
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	lw	s0, 8(sp)                       # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end4:
+	.size	getString, .Lfunc_end4-getString
+	.cfi_endproc
+                                        # -- End function
+	.globl	getInt                          # -- Begin function getInt
+	.p2align	2
+	.type	getInt,@function
+getInt:                                 # @getInt
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	sw	s0, 8(sp)                       # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 16
+	.cfi_def_cfa s0, 0
+	lui	a0, %hi(.L.str.2)
+	addi	a0, a0, %lo(.L.str.2)
+	addi	a1, s0, -12
+	call	scanf
+	lw	a0, -12(s0)
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	lw	s0, 8(sp)                       # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end5:
+	.size	getInt, .Lfunc_end5-getInt
+	.cfi_endproc
+                                        # -- End function
+	.globl	toString                        # -- Begin function toString
+	.p2align	2
+	.type	toString,@function
+toString:                               # @toString
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -16
+	.cfi_def_cfa_offset 16
+	sw	ra, 12(sp)                      # 4-byte Folded Spill
+	sw	s0, 8(sp)                       # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 16
+	.cfi_def_cfa s0, 0
+	sw	a0, -12(s0)
+	li	a0, 16
+	li	a1, 0
+	call	malloc
+	sw	a0, -16(s0)
+	lw	a0, -16(s0)
+	lw	a2, -12(s0)
+	lui	a1, %hi(.L.str.2)
+	addi	a1, a1, %lo(.L.str.2)
+	call	sprintf
+	lw	a0, -16(s0)
+	lw	ra, 12(sp)                      # 4-byte Folded Reload
+	lw	s0, 8(sp)                       # 4-byte Folded Reload
+	addi	sp, sp, 16
+	ret
+.Lfunc_end6:
+	.size	toString, .Lfunc_end6-toString
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.substring                # -- Begin function string.substring
+	.p2align	2
+	.type	string.substring,@function
+string.substring:                       # @string.substring
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -48
+	.cfi_def_cfa_offset 48
+	sw	ra, 44(sp)                      # 4-byte Folded Spill
+	sw	s0, 40(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 48
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	sw	a1, -20(s0)
+	sw	a2, -24(s0)
+	lw	a0, -24(s0)
+	lw	a1, -20(s0)
+	sub	a0, a0, a1
+	addi	a0, a0, 1
+	srai	a1, a0, 31
+	call	malloc
+	sw	a0, -32(s0)
+	lw	a0, -20(s0)
+	sw	a0, -36(s0)
+	j	.LBB7_1
+.LBB7_1:                                # =>This Inner Loop Header: Depth=1
+	lw	a0, -36(s0)
+	lw	a1, -24(s0)
+	bge	a0, a1, .LBB7_4
+	j	.LBB7_2
+.LBB7_2:                                #   in Loop: Header=BB7_1 Depth=1
+	lw	a0, -16(s0)
+	lw	a1, -36(s0)
+	add	a0, a0, a1
+	lb	a0, 0(a0)
+	lw	a2, -32(s0)
+	lw	a3, -20(s0)
+	sub	a1, a1, a3
+	add	a1, a2, a1
+	sb	a0, 0(a1)
+	j	.LBB7_3
+.LBB7_3:                                #   in Loop: Header=BB7_1 Depth=1
+	lw	a0, -36(s0)
+	addi	a0, a0, 1
+	sw	a0, -36(s0)
+	j	.LBB7_1
+.LBB7_4:
+	lw	a0, -32(s0)
+	lw	a1, -24(s0)
+	lw	a2, -20(s0)
+	sub	a1, a1, a2
+	add	a0, a0, a1
+	sb	zero, 0(a0)
+	lw	a0, -32(s0)
+	lw	ra, 44(sp)                      # 4-byte Folded Reload
+	lw	s0, 40(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 48
+	ret
+.Lfunc_end7:
+	.size	string.substring, .Lfunc_end7-string.substring
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.parseInt                 # -- Begin function string.parseInt
+	.p2align	2
+	.type	string.parseInt,@function
+string.parseInt:                        # @string.parseInt
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -32
+	.cfi_def_cfa_offset 32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	s0, 24(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 32
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	lw	a0, -16(s0)
+	lui	a1, %hi(.L.str.2)
+	addi	a1, a1, %lo(.L.str.2)
+	addi	a2, s0, -20
+	call	sscanf
+	lw	a0, -20(s0)
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	lw	s0, 24(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end8:
+	.size	string.parseInt, .Lfunc_end8-string.parseInt
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.ord                      # -- Begin function string.ord
+	.p2align	2
+	.type	string.ord,@function
+string.ord:                             # @string.ord
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -32
+	.cfi_def_cfa_offset 32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	s0, 24(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 32
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	sw	a1, -20(s0)
+	lw	a0, -16(s0)
+	lw	a1, -20(s0)
+	add	a0, a0, a1
+	lb	a0, 0(a0)
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	lw	s0, 24(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end9:
+	.size	string.ord, .Lfunc_end9-string.ord
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.add                      # -- Begin function string.add
+	.p2align	2
+	.type	string.add,@function
+string.add:                             # @string.add
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -48
+	.cfi_def_cfa_offset 48
+	sw	ra, 44(sp)                      # 4-byte Folded Spill
+	sw	s0, 40(sp)                      # 4-byte Folded Spill
+	sw	s1, 36(sp)                      # 4-byte Folded Spill
+	sw	s2, 32(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	.cfi_offset s1, -12
+	.cfi_offset s2, -16
+	addi	s0, sp, 48
+	.cfi_def_cfa s0, 0
+	sw	a0, -24(s0)
+	sw	a1, -32(s0)
+	lw	a0, -24(s0)
+	call	strlen
+	mv	s1, a0
+	mv	s2, a1
+	lw	a0, -32(s0)
+	call	strlen
+	add	a1, s2, a1
+	add	a2, s1, a0
+	sltu	a0, a2, s1
+	add	a1, a1, a0
+	addi	a0, a2, 1
+	sltu	a2, a0, a2
+	add	a1, a1, a2
+	call	malloc
+	sw	a0, -40(s0)
+	lw	a0, -40(s0)
+	lw	a1, -24(s0)
+	call	strcpy
+	lw	a0, -40(s0)
+	lw	a1, -32(s0)
+	call	strcat
+	lw	a0, -40(s0)
+	lw	ra, 44(sp)                      # 4-byte Folded Reload
+	lw	s0, 40(sp)                      # 4-byte Folded Reload
+	lw	s1, 36(sp)                      # 4-byte Folded Reload
+	lw	s2, 32(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 48
+	ret
+.Lfunc_end10:
+	.size	string.add, .Lfunc_end10-string.add
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.lt                       # -- Begin function string.lt
+	.p2align	2
+	.type	string.lt,@function
+string.lt:                              # @string.lt
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -32
+	.cfi_def_cfa_offset 32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	s0, 24(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 32
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	sw	a1, -24(s0)
+	lw	a0, -16(s0)
+	lw	a1, -24(s0)
+	call	strcmp
+	srli	a0, a0, 31
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	lw	s0, 24(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end11:
+	.size	string.lt, .Lfunc_end11-string.lt
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.le                       # -- Begin function string.le
+	.p2align	2
+	.type	string.le,@function
+string.le:                              # @string.le
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -32
+	.cfi_def_cfa_offset 32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	s0, 24(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 32
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	sw	a1, -24(s0)
+	lw	a0, -16(s0)
+	lw	a1, -24(s0)
+	call	strcmp
+	slti	a0, a0, 1
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	lw	s0, 24(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end12:
+	.size	string.le, .Lfunc_end12-string.le
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.gt                       # -- Begin function string.gt
+	.p2align	2
+	.type	string.gt,@function
+string.gt:                              # @string.gt
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -32
+	.cfi_def_cfa_offset 32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	s0, 24(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 32
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	sw	a1, -24(s0)
+	lw	a0, -16(s0)
+	lw	a1, -24(s0)
+	call	strcmp
+	sgtz	a0, a0
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	lw	s0, 24(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end13:
+	.size	string.gt, .Lfunc_end13-string.gt
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.ge                       # -- Begin function string.ge
+	.p2align	2
+	.type	string.ge,@function
+string.ge:                              # @string.ge
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -32
+	.cfi_def_cfa_offset 32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	s0, 24(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 32
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	sw	a1, -24(s0)
+	lw	a0, -16(s0)
+	lw	a1, -24(s0)
+	call	strcmp
+	not	a0, a0
+	srli	a0, a0, 31
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	lw	s0, 24(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end14:
+	.size	string.ge, .Lfunc_end14-string.ge
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.eq                       # -- Begin function string.eq
+	.p2align	2
+	.type	string.eq,@function
+string.eq:                              # @string.eq
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -32
+	.cfi_def_cfa_offset 32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	s0, 24(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 32
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	sw	a1, -24(s0)
+	lw	a0, -16(s0)
+	lw	a1, -24(s0)
+	call	strcmp
+	seqz	a0, a0
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	lw	s0, 24(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end15:
+	.size	string.eq, .Lfunc_end15-string.eq
+	.cfi_endproc
+                                        # -- End function
+	.globl	string.ne                       # -- Begin function string.ne
+	.p2align	2
+	.type	string.ne,@function
+string.ne:                              # @string.ne
+	.cfi_startproc
+# %bb.0:
+	addi	sp, sp, -32
+	.cfi_def_cfa_offset 32
+	sw	ra, 28(sp)                      # 4-byte Folded Spill
+	sw	s0, 24(sp)                      # 4-byte Folded Spill
+	.cfi_offset ra, -4
+	.cfi_offset s0, -8
+	addi	s0, sp, 32
+	.cfi_def_cfa s0, 0
+	sw	a0, -16(s0)
+	sw	a1, -24(s0)
+	lw	a0, -16(s0)
+	lw	a1, -24(s0)
+	call	strcmp
+	snez	a0, a0
+	lw	ra, 28(sp)                      # 4-byte Folded Reload
+	lw	s0, 24(sp)                      # 4-byte Folded Reload
+	addi	sp, sp, 32
+	ret
+.Lfunc_end16:
+	.size	string.ne, .Lfunc_end16-string.ne
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object                  # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"%s"
+	.size	.L.str, 3
+
+	.type	.L.str.1,@object                # @.str.1
+.L.str.1:
+	.asciz	"%s\n"
+	.size	.L.str.1, 4
+
+	.type	.L.str.2,@object                # @.str.2
+.L.str.2:
+	.asciz	"%d"
+	.size	.L.str.2, 3
+
+	.type	.L.str.3,@object                # @.str.3
+.L.str.3:
+	.asciz	"%d\n"
+	.size	.L.str.3, 4
+
+	.ident	"Ubuntu clang version 15.0.7"
+	.section	".note.GNU-stack","",@progbits
